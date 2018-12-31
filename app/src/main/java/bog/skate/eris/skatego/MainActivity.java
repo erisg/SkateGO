a3226a83be14ee430e510acd300a05ac66daf637
geral <erisbg.513@gmail.com>
2018-12-31 18:32:53 -0500

    subirfoto

--- app/src/main/java/bog/skate/eris/skatego/MainActivity.java
+++ app/src/main/java/bog/skate/eris/skatego/MainActivity.java
@@ -2,15 +2,22 @@ package bog.skate.eris.skatego;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+
+   ImageView foto,foti;
 
 
     @Override
@@ -18,9 +25,14 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        foto= (ImageView) findViewById(R.id.foto);
+        foti=(ImageView) findViewById(R.id.foti);
+
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
+
+
 
     }
 
@@ -44,5 +56,26 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
 
         return false;
+    }
+
+    public void onclick(View view) {
+        cargarImagen();
+    }
+
+    private void cargarImagen() {
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicacion"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK){
+            Uri path=data.getData();
+            foto.setImageURI(path);
+
+
+        }
     }
 }
