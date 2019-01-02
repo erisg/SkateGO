1364ce4ff974b3a5bc7e791d24f55ddee1f4591a
geral <erisbg.513@gmail.com>
2019-01-02 14:16:09 -0500

    c

--- app/src/main/java/bog/skate/eris/skatego/MainActivity.java
+++ app/src/main/java/bog/skate/eris/skatego/MainActivity.java
@@ -58,24 +58,4 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         return false;
     }
 
-    public void onclick(View view) {
-        cargarImagen();
-    }
-
-    private void cargarImagen() {
-        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/");
-        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicacion"),10);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode==RESULT_OK){
-            Uri path=data.getData();
-            foto.setImageURI(path);
-
-
-        }
-    }
 }
